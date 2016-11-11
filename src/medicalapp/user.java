c579bc9c877845d86410ac76f3c4d309e453087f
lehighbrownandbyte <hms218@lehigh.edu>
2016-11-11 11:20:03 -0500

    Create user.java

---
+++ src/medicalapp/user.java
@@ -0,0 +1,22 @@
+/**
+ * @author JonathanBlum
+ * CSE 216 - user class
+ */
+
+public class User{
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String toString() {
+        return "User{" + "id=" + id + '}';
+    }
+       
+  
+}
